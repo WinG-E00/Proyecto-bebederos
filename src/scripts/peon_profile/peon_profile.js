577ac59462b0091e6a83465240daa808fc4fed4b
WinjeKN <gastonencinawj@gmail.com>
2026-06-03 19:31:36 -0300

    dev:prueba conexion con base de datos

--- src/scripts/peon_profile/peon_profile.js
+++ src/scripts/peon_profile/peon_profile.js
@@ -16,6 +16,95 @@ async function obtenerUsuarios() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+// COnsulta para obtener ubicaciones
+async function obtenerUbicaciones() {
+    try {
+        const resultado = await pool.query(`
+            SELECT
+                id,
+                name,
+                latitude,
+                longitude
+            FROM water_thoughts
+            WHERE is_active = TRUE
+        `);
+
+        console.log(resultado.rows);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//Consulta para obtener la cantidad de bebederos
+async function obtenerCantidadBebederos() {
+    try {
+        const resultado = await pool.query(`
+            SELECT COUNT(*) AS total
+            FROM water_troughs
+            WHERE is_active = TRUE
+        `);
+
+        console.log(resultado.rows[0]);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//consulta para obetener el nivel de agua de cada bebedero
+async function obtenerNivelesAgua() {
+    try {
+        const resultado = await pool.query(`
+            SELECT
+                id,
+                name,
+                water_level,
+                capacity_liters
+            FROM water_thoughts
+            WHERE is_active = TRUE
+        `);
+
+        console.log(resultado.rows);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//consulta para obtener la informacion completa de el bebedero
+async function obtenerBebederos() {
+    try {
+        const resultado = await pool.query(`
+            SELECT
+                id,
+                name,
+                zone_or_paddock,
+                latitude,
+                longitude,
+                water_level,
+                capacity_liters
+            FROM water_thoughts
+            WHERE is_active = TRUE
+        `);
+
+        console.log(resultado.rows);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+
 //Datos que van a requerir las consultas
 //Nivel de agua de cada registro de los bebederos
 //Cantidad de bebederos
