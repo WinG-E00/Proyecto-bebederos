01e130a37265274da2ca41c6587a098aaf0672dc
WinG-E00 <gastonencinawj@gmail.com>
2026-06-29 16:16:15 -0300

    dev: Creacion de models de la base de datos, creacion del config, cracion de las proximas routas config que usaremos

---
+++ js/database/models/proyecto_bebedores_db.js
@@ -0,0 +1,70 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+
+
+//Tabla de usuarios
+export const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  gmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true, // Evita que se repitan correos
+    validate: {
+      isEmail: true // Valida que tenga estructura de correo válida
+    }
+  },
+  nombreCompleto: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    // Puedes mapearlo a snake_case en la BD si prefieres: field: 'nombre_completo'
+  },
+  contraseña: {
+    type: DataTypes.STRING,
+    allowNull: false
+    // Recuerda que en producción es buena práctica guardar el hash (ej. con bcrypt) y no texto plano
+  },
+  rol: {
+    type: DataTypes.ENUM('peon', 'dueño'),
+    allowNull: false,
+    defaultValue: 'peon' // Opción por defecto si no se especifica
+  }
+}, {
+  tableName: 'usuarios' // Fuerza a que la tabla en MySQL se llame exactamente así
+});
+
+
+
+export const Sensor = sequelize.define('Sensor', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  latitude: {
+    type: DataTypes.DECIMAL(10, 8), // Coordenadas GPS de alta precisión
+    allowNull: false
+  },
+  longitude: {
+    type: DataTypes.DECIMAL(11, 8),
+    allowNull: false
+  },
+  water_level: {
+    type: DataTypes.INTEGER, // Porcentaje (0 a 100)
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 100
+    }
+  }
+}, { 
+  tableName: 'sensores',
+  timestamps: true // Te guarda automáticamente la fecha y hora de cada medición
+});
+
+
+
