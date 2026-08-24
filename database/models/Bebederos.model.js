c29ae4d0b627ddd0ad8704dac12d14eba8d23c5f
WinG-E00 <gastonencinawj@gmail.com>
2026-08-24 17:56:16 -0300

    database: Se agrego modelo para bebederos para perfil y archivo de relaciones

---
+++ database/models/Bebederos.model.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js'; // Ajusta la ruta a tu archivo de conexión de Sequelize
+
+const Bebederos = sequelize.define('Bebederos', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  water_level: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  longitud: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  latitud: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  }
+}, {
+  tableName: 'bebederos',
+  timestamps: true
+});
+
+export default Bebederos;
---
+++ database/models/Perfil.mode.js
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize';
+// Asegúrate de importar tu instancia de sequelize desde tu archivo de conexión
+import { sequelize } from '../config/database.js'; // Ajusta la ruta según tu proyecto
+
+const Perfil = sequelize.define('Perfil', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  perfil: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'perfiles',
+  timestamps: true
+});
+
+export default Perfil;
