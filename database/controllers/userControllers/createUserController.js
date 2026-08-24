9bbef57fb08adf42c92a2203025c3b04f05bb467
WinG-E00 <gastonencinawj@gmail.com>
2026-08-24 18:35:39 -0300

    database: creacion de controller para registrar usuario

---
+++ database/controllers/userControllers/createUserController.js
@@ -0,0 +1,38 @@
+import User from '../models/User.model.js';
+// Controlador para crear usuario
+// Se supone que aca podes crear el usuario, este controllador sera usado para el router que permite hacer register
+
+export const createUserControll = async (req, res) => {
+
+    try {
+        const { user, gmail, password, perfilId } = await req.body;
+
+        //Faltan crear las validaciones
+
+
+        //Creacion
+        const nuevoUsuario = await User.create({
+            user,
+            gmail,
+            password,
+            perfilId
+        });
+
+
+        return res.status(201).json({
+            mensaje: 'Usuario creado con éxito',
+            usuario: nuevoUsuario
+        });
+
+    } catch(err) {
+        return res.status(500).json({
+      mensaje: 'Error al crear el usuario',
+      error: error.message }
+}
+
+
+};
+
+
+
+
